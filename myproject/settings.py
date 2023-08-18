ad16570ccb99934f0ff91851c694ab8ea76cee1e
Bishal721 <shresthabishal721@gmail.com>
2023-08-18 09:41:58 +0545

    Added whitenose

--- myproject/settings.py
+++ myproject/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -134,6 +135,10 @@ STATIC_URL = 'public/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'public')
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
